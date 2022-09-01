5625c7e852d886f798792bd0282c148602dfa585
106128109 <106128109@qq.com>
2022-09-02 01:37:47 +0800

    init

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,13 @@
+<?php
+// 数据库配置
+return array(
+	'DB_TYPE'   => 'mysql',       // 数据库类型
+	'DB_HOST'   => '127.0.0.1',       // 服务器地址
+	'DB_NAME'   => 'yuanbanqdudu_cn',       // 数据库名
+	'DB_USER'   => 'yuanbanqdudu_cn',       // 用户名
+	'DB_PWD'    => 'Rd55j2eNHWHbLhTS',        // 密码
+	'DB_PORT'   => '3306',       // 端口
+	'DB_PREFIX' => 'pay_',     // 数据库表前缀
+	'SYS_KEY'    => 'SN20180824202337', 
+);
+
---
+++ Application/Common/Conf/website.php
@@ -0,0 +1,10 @@
+<?php 
+		return array(
+			'WEB_TITLE' => '聚合支付',
+			'DOMAIN' => '143.92.58.230:8755',
+			'MODULE_ALLOW_LIST'   => array('Home','User','Admin','Install', 'Weixin','Pay','Cashier','Agent','Payment','Code'),
+			'URL_MODULE_MAP'  => array('admin'=>'admin', 'agent'=>'user', 'user'=>'user', 'code'=>'code'),
+			'LOGINNAME' => 'Login',
+			'HOUTAINAME' => 'admin',
+		);
+?>
